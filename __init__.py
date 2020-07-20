eb4778aa8ddf09e38d3e371c95160442c523e555
Charles Doebler <cdoebler@cannon.local>
2020-07-19 20:15:22 -0400

    clean timer

--- __init__.py
+++ __init__.py
@@ -1,21 +1,12 @@
-from mycroft import MycroftSkill
 import os
 import time
 
 
-class Winston_monitoring(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
-
-    def measure_temp(self):
-        temp = os.popen("vcgencmd measure_temp").readline()
-        return (temp.replace("temp=", ""))
-
-        while True:
-            print(temp())
-            self.speak("My current core temperature is {} degress celsius.".format(temp))
-            time.sleep(15)
+def measure_temp():
+    temp = os.popen("vcgencmd measure_temp").readline()
+    return (temp.replace("temp=", ""))
 
 
-def create_skill():
-    return Winston_monitoring()
+while True:
+    print(measure_temp())
+    time.sleep(15)
